18e4d89dc306b38a50e5fcbcedac89b94314163d
unknown <shirley.mramirez@yahoo.com>
2018-08-15 21:47:36 -0700

    initial commit

---
+++ src/components/Posts.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  // used fetch request as api and fetch will return a promise
+  // that will give as a result that will map to json
+  // another .then that will give as a data
+  componentWillMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => this.setState({ posts: data }));
+  }
+
+  render() {
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
+    return (
+      <div>
+        <h1>Hello World</h1>
+        {postItems}
+      </div>
+    );
+  }
+}
+
+export default Posts;
