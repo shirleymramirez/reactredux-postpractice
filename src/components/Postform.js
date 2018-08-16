bfd1fbbdb02572ff3bbf29e9a389d199953c6906
unknown <shirley.mramirez@yahoo.com>
2018-08-15 22:13:52 -0700

    added fetch method and event handler for the form

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -8,17 +8,36 @@ class PostForm extends Component {
       body: ""
     };
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+
   render() {
     return (
       <div>
         <h1>Add Posts</h1>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div>
             <label>Title: </label>
             <br />
