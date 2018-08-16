493a3720132389de88d79756e85ec7bfda1a5be0
unknown <shirley.mramirez@yahoo.com>
2018-08-16 15:11:39 -0700

    added reducer and actions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 
 import Posts from "./components/Posts";
 import PostForm from "./components/Postform";
 
-const store = createStore(() => [], {}, applyMiddleware());
+import store from "./store";
 
 class App extends Component {
   render() {
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,24 +1,14 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
 
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    };
-  }
-
-  // used fetch request as api and fetch will return a promise
-  // that will give as a result that will map to json
-  // another .then that will give as a data
   componentWillMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(data => this.setState({ posts: data }));
+    this.props.fetchPosts();
   }
 
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -33,4 +23,11 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+const mapStatToProps = state => ({
+  posts: state.posts.items
+});
+
+export default connect(
+  mapStatToProps,
+  { fetchPosts }
+)(Posts);
