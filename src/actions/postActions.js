4a6031a853bd8ad9486a25bd17b822ba57f569aa
unknown <shirley.mramirez@yahoo.com>
2018-08-17 09:37:24 -0700

    added redux devtools setting in store.js

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,7 +1,6 @@
 import { FETCH_POSTS, NEW_POSTS } from "./types";
 
 export const fetchPosts = () => dispatch => {
-  console.log("fetching");
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then(res => res.json())
     .then(posts =>
