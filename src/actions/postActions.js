f7ad38ffcb2cf65bb5fb7bff9dca29771565105c
unknown <shirley.mramirez@yahoo.com>
2018-08-17 09:54:20 -0700

    added createPost

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POSTS } from "./types";
+import { FETCH_POSTS, NEW_POST } from "./types";
 
 export const fetchPosts = () => dispatch => {
   fetch("https://jsonplaceholder.typicode.com/posts")
@@ -7,6 +7,24 @@ export const fetchPosts = () => dispatch => {
       dispatch({
         type: FETCH_POSTS,
         payload: posts
+      })
+    );
+};
+
+export const createPost = postData => dispatch => {
+  console.log("action called");
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(postData)
+  })
+    .then(res => res.json())
+    .then(post =>
+      dispatch({
+        type: NEW_POST,
+        payload: post
       })
     );
 };
--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createPost } from "../actions/postActions";
 
 class PostForm extends Component {
   constructor(props) {
@@ -22,15 +25,8 @@ class PostForm extends Component {
       body: this.state.body
     };
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(post)
-    })
-      .then(res => res.json())
-      .then(data => console.log(data));
+    // call action
+    this.props.createPost(post);
   }
 
   render() {
@@ -66,4 +62,11 @@ class PostForm extends Component {
   }
 }
 
-export default PostForm;
+PostForm.PropTypes = {
+  createPost: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createPost }
+)(PostForm);
